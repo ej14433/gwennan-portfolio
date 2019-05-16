d733f6510bdb814b3d7050f46bf03799a28d3c42
Elis Jones <ej14433@my.bristol.ac.uk>
2019-05-16 15:03:28 +0100

    circular elements structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,19 @@ import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { Circle, Item } from "./style"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    Small Change
+    <Circle className="circle" items={6}>
+      <div className="center" />
+      {[0, 1, 2, 3, 4, 5, 6, 7].map(n => (
+        <Item position={n}>
+          <div className="content" />
+        </Item>
+      ))}
+    </Circle>
   </Layout>
 )
 
---
+++ src/pages/style.js
@@ -0,0 +1,52 @@
+import styled from "styled-components"
+
+export const Circle = styled.div`
+  width: 24rem;
+  height: 24rem;
+  position: relative;
+  border-radius: 50%;
+  margin: 5rem auto 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .center {
+    background: blue;
+    height: 15rem;
+    width: 15rem;
+    border-radius: 50%;
+    transition: all 0.3s ease-in-out;
+    margin-left: 15px;
+    margin-top: 15px;
+
+    :hover {
+      opacity: 0.5;
+    }
+  }
+`
+export const Item = styled.div`
+  height: ${({ center }) => (center ? "20rem" : "5rem")};
+  width: ${({ center }) => (center ? "20rem" : "5rem")};
+  position: absolute;
+  top: ${({ center }) => (center ? "18.5%" : "50%")};
+  left: ${({ center }) => (center ? "18.5%" : "50%")};
+  margin: -2rem;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transform: rotate(${({ position }) => position * 45}deg) translate(12rem)
+    rotate(-${({ position }) => position * 45}deg);
+
+  .content {
+    background: grey;
+    height: 5rem;
+    width: 5rem;
+    border-radius: 50%;
+    transition: 0.3s ease-in-out all;
+    :hover {
+      opacity: 0.5;
+    }
+  }
+`
