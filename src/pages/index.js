e1ba4e057c4d84f6efe84a459d84f5df46b3f649
Elis Jones <ej14433@my.bristol.ac.uk>
2019-05-16 14:01:31 +0100

    deploy preview test

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,20 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hey!</h1>
-    <p>I am in the middle of updating my site! </p>
-    <p>
-      Find me on LinkedIn{" "}
-      <a
-        href="https://www.linkedin.com/in/gwennan-dafis/"
-        rel="noopener noreferrer"
-      >
-        Gwennan Dafis
-      </a>
-    </p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    Small Change
   </Layout>
 )
 
